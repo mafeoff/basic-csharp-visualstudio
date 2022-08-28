4f3c9dbe9256d3cdacd9dbcdc5f0aa66b3ae6015
Mafe <contato.mf.13@gmail.com>
2022-08-28 11:50:00 -0300

    Add mathematics class and methods exercises

---
+++ individual-exercises/metodos-classe-matematica-classe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal class Matematica
+    {
+        public void Triplo(int numTri)
+        {
+            int tri = numTri * 3;
+            MessageBox.Show("O triplo é:  " + tri);
+        }
+        public void Soma(float num1, float num2)
+        {
+            float soma = num1 + num2;
+            MessageBox.Show("A soma é: " + soma);
+        }
+        public void Quadrado(float numQ)
+        {
+            float quad = numQ * numQ;
+            MessageBox.Show("O quadrado  é " + quad);
+        }
+    }
+}
---
+++ individual-exercises/metodos-classe-matematica-form.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMetodo_Click(object sender, EventArgs e)
+        {
+            int numTri = int.Parse(txt1.Text);
+            //espressão da instancia da calasse
+            Matematica mat = new Matematica();
+            mat.Triplo(numTri);
+
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            float num1 = float.Parse(txtNum1.Text);
+            float num2 = float.Parse(txtNum2.Text);
+
+            Matematica mat = new Matematica();
+            mat.Soma(num1, num2);
+
+        }
+
+        private void btnQuadrado_Click(object sender, EventArgs e)
+        {
+            float numQ = float.Parse(txtQuadrado.Text);
+
+            Matematica mat = new Matematica();   
+            mat.Quadrado(numQ);
+
+        }
+    }
+}
