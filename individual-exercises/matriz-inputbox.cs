6c167908c8f0c6aa2a000bc7874b581fc2653a2a
Mafe <contato.mf.13@gmail.com>
2022-11-03 08:10:00 -0300

    Add matrix input exercise

---
+++ individual-exercises/matriz-inputbox.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace matriz
+{
+    public partial class Matriz : Form
+    {
+        public Matriz()
+        {
+            InitializeComponent();
+        }
+
+        private void btnComeco_Click(object sender, EventArgs e)
+        {
+            int[,] matrizesNumeros = new int[3, 3];
+            for (int i = 0; i<3;i++)
+            {
+                for (int j = 0;  j < 3; j++)
+                {
+                    String valor = Interaction.InputBox("Insira nove elementos e obtenha uma matrix 3X3", "Matrizes");
+                    matrizesNumeros[i, j] = int.Parse(valor);
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    lblResultado.Text += matrizesNumeros[i,j] + " ";
+                }
+                lblResultado.Text += "\n";
+            }
+        }
+    }
+}
